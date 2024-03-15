b14f356e497c08334be4162d6424e89aa7809b96
Aditya <35306205+adi0900@users.noreply.github.com>
2024-03-15 21:58:40 +0530

    Converstion Of Data Types

---
+++ 01_Basics/03_datatypeConversion.js
@@ -0,0 +1,24 @@
+"use strict";  //New JS Library Usage
+let age = "18abc"
+
+console.log("aditya");
+console.log(typeof age);
+
+let valueInNumber = Number(age)   //Convert String to Number 
+console.log(typeof valueInNumber);
+console.log(valueInNumber);     //NaN Conversion 
+
+//"33" => 33
+//"33abc" => NaN
+// True => 1
+// False => 0
+
+let isLoggedIn = 1   // 1=>True
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let someNumber = "33"
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
