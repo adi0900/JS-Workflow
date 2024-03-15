3bd8ef023c3849d61d31f64e47adaaebef44667c
Aditya <35306205+adi0900@users.noreply.github.com>
2024-03-15 23:45:00 +0530

    Comperision of Operators

---
+++ 01_Basics/05_Comparision.js
@@ -0,0 +1,20 @@
+//console.log(2 > 1);
+//console.log(2 < 1);
+//console.log(2 >= 1);
+//console.log(2 != 1);
+//console.log(2 == 1);
+
+console.log("2" > 1);
+console.log("02" > 2);
+
+console.log(null >= 0);  // here NULL is Converted to 0 so it is = to 0
+
+console.log(undefined == 0);
+console.log(null == undefined); 
+//or
+console.log(undefined == null);
+
+// === Strict Check
+
+console.log("2" === 2);  // == --> check for value === Check data type also
+
