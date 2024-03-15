d453cea980559543f27fcbf181a6404ce636d75e
Aditya <35306205+adi0900@users.noreply.github.com>
2024-03-15 18:14:47 +0530

    Using New JS

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";  //Treat all Js as newer version
+
+// alert(3+3);  //using Node JS
+
+console.log( 3+3 );
+console.log("Aditya");
+
+let name = "adi"  //String
+let age = 18 //Int
+let isLoggingData = false //Boolean
+
+// number => 2^53
+// bigint  BIG INT DATA TYPE
+// string -> ""
+// boolean -> true or false
+// null => Empty value but Assigned 
+// undefined => Not Assigned 
+// Symbol => unique
+
+// Object 
+
+console.log(typeof undefined);
+console.log(typeof null);
