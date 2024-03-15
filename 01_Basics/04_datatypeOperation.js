bb2ec4c94aaa7e36b58c2d373a22d1ea64ff2352
Aditya <35306205+adi0900@users.noreply.github.com>
2024-03-15 22:12:39 +0530

    Learning Operations in js

---
+++ 01_Basics/04_datatypeOperation.js
@@ -0,0 +1,36 @@
+//******************OPERATIONS**********************//
+
+let value = 3
+let negValue = -value  //Convert Value 
+// console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+
+let str1 = "hello\n"
+let str2 = "aditya"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
