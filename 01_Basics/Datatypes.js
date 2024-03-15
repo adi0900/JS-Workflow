e41f55a46f1fdb5b841f2b04101a23db238cb5ea
Aditya <35306205+adi0900@users.noreply.github.com>
2024-03-16 00:00:20 +0530

    Datatypes Summary

---
+++ 01_Basics/Datatypes.js
@@ -0,0 +1,40 @@
+// Primitive --> 7 : string : Number : boolean : NULL : undefined : Symbol : BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+// const isLoggedIn = false
+// let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId);
+
+//const bigNumber = 2343253256546523523n
+
+const heros = ["Aditya" , "Gojo" , "Goku"] 
+let myObj =
+{
+    name: "Adi",
+    age: 22,
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+}
+
+/*1) Primitive Datatypes
+Number => number
+String  => string
+Boolean  => boolean
+null  => object
+undefined  =>  undefined
+Symbol  =>  symbol
+BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+Arrays  =>  object
+Function  =>  function
+Object  =>  object
+
+*/
+
+// Reference (Non Primitive) : Array , Objects , Functions
