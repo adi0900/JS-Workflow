f89ca79cde8c9874382ff680777e010807da8e50
Aditya <35306205+adi0900@users.noreply.github.com>
2024-03-16 16:12:56 +0530

    Strings and Maths

---
+++ 01_Basics/07_numsandmaths.js
@@ -0,0 +1,36 @@
+// +++++++++++++ Numbers and Maths ++++++++++++++++++++++++//
+
+const number = 400 
+console.log(number);
+
+const balance = new Number(100)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));         // Show After String 
+
+const otherNumber = 123.9888
+console.log(otherNumber.toPrecision(3));
+
+const newNumber = 1000000
+console.log(newNumber.toLocaleString('en-IN'));
+
+
+
+// +++++++++++++++++ MATH +++++++++++++++++++++ //
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.6));
+console.log(Math.min(4,3,6,8));
+console.log(Math.max(4,3,6,8));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
